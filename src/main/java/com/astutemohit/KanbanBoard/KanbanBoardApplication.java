e93572cdb14022f6f2a64336a80c217ed6d93992
MOHITNEGI999 <mohitneg377@gmail.com>
2023-09-23 16:17:51 +0530

    Project setup with code initializer

---
+++ src/main/java/com/astutemohit/KanbanBoard/KanbanBoardApplication.java
@@ -0,0 +1,13 @@
+package com.astutemohit.KanbanBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astutemohit/KanbanBoard/KanbanBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astutemohit.KanbanBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
